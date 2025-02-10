e0b61ae53313bda9038796f5a6e95be86e0bd219
eomcri <jihun.eomcri@gmail.com>
2025-02-10 20:20:28 +0900

    add gamemode

---
+++ Source/CharacterClass/Public/EomcriGameMode.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "EomcriGameMode.generated.h"
+
+UCLASS()
+class CHARACTERCLASS_API AEomcriGameMode : public AGameMode
+{
+	GENERATED_BODY()
+};
